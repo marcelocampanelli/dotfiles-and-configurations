960cdfae7fc682107f1a4102d0759b9476921695
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2026-07-26 14:23:56 -0300

    update cursor config

--- cursor/extensions.sh
+++ cursor/extensions.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 extensions=(
-  "anysphere.remote-containers"
-  "anysphere.remote-ssh"
-  "dbaeumer.vscode-eslint"
-  "eamodio.gitlens"
-  "esbenp.prettier-vscode"
-  "golang.go"
-  "hverlin.mise-vscode"
-  "ms-azuretools.vscode-containers"
-  "ms-azuretools.vscode-docker"
-  "pkief.material-icon-theme"
-  "rvest.vs-code-prettier-eslint"
-  "shopify.ruby-lsp"
-  "tombi-toml.tombi"
+  "anysphere.remote-containers",
+  "anysphere.remote-ssh",
+  "bradlc.vscode-tailwindcss",
+  "dbaeumer.vscode-eslint",
+  "eamodio.gitlens",
+  "esbenp.prettier-vscode",
+  "golang.go",
+  "hverlin.mise-vscode",
+  "ms-azuretools.vscode-containers",
+  "ms-azuretools.vscode-docker",
+  "nebula-themes.nebula-aura-theme",
+  "openai.chatgpt",
+  "pkief.material-icon-theme",
+  "rvest.vs-code-prettier-eslint",
+  "shd101wyy.markdown-preview-enhanced",
+  "shopify.ruby-lsp",
+  "tombi-toml.tombi",
+  "vue.volar",
   "yoavbls.pretty-ts-errors"
 )
 
